7522d3160b2b94540aac7caad752c8505f1d179c
yinchak <57385082+yinchak@users.noreply.github.com>
2025-04-11 10:08:23 +0800

    Update sensor.py

--- custom_components/energy_tracker/sensor.py
+++ custom_components/energy_tracker/sensor.py
@@ -1,10 +1,11 @@
 from homeassistant.components.sensor import SensorEntity
 
 class TotalOnTimeSensor(SensorEntity):
-    def __init__(self, name, value):
+    def __init__(self, name, value, entry_id):
         self._state = value
         self._name = f"Energy Tracker {name.replace('_', ' ').title()}"
-        self._unique_id = f"energy_tracker_{name}"
+        self._unique_id = f"energy_tracker_{entry_id}_{name}"
+        self._attr_should_poll = False
 
     @property
     def name(self):
@@ -23,10 +24,11 @@ class TotalOnTimeSensor(SensorEntity):
         return self._unique_id
 
 class DailyOnTimeSensor(SensorEntity):
-    def __init__(self, name, value):
+    def __init__(self, name, value, entry_id):
         self._state = value
         self._name = f"Energy Tracker {name.replace('_', ' ').title()}"
-        self._unique_id = f"energy_tracker_{name}"
+        self._unique_id = f"energy_tracker_{entry_id}_{name}"
+        self._attr_should_poll = False
 
     @property
     def name(self):
@@ -39,6 +41,15 @@ class DailyOnTimeSensor(SensorEntity):
     @property
     def unit_of_measurement(self):
         return "minutes"
+
+    @property
+    def unique_id(self):
+        return self._unique_id
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+    """Set up the sensor platform."""
+    sensors = entry.runtime_data
+    async_add_entities(sensors)
 
     @property
     def unique_id(self):
